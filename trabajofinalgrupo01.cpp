65f184ff9a59d2d8d44d53f983b958f82712323c
Kristhyan <kis.ger_94@hotmail.com>
2015-01-24 14:02:38 -0500

    avance 02
    elaboracion del primer literal

--- trabajofinalgrupo01.cpp
+++ trabajofinalgrupo01.cpp
@@ -1,4 +1,5 @@
 # include <conio.h>
+#include <iostream.h>
 main()
 {
 	int s,r=1;
@@ -15,17 +16,53 @@ main()
 		gotoxy(10,10), cprintf("1. GENERAR TABLAS DE VERDAD");
 		gotoxy(10,12), cprintf("2. VER TABLAS CON DOS ENTRADAS");
 		gotoxy(10,14), cprintf("3. GENERAR Y VER TABLAS CON DOS ENTRADAS");
-		gotoxy(10,16), cprintf("4. Mostrar");
 		gotoxy(10,18), cprintf("4. Salir ");
 		textcolor(GREEN);
-		gotoxy(10,20), cprintf("¨OPERACION A REALIZAR");
+		gotoxy(10,20), cprintf("OPERACION A REALIZAR");
 		gotoxy(28,20);
       seleccion=getche();
 		textcolor(YELLOW);
 	gotoxy(28,20);
 		switch(seleccion)
 		{
-			case '1': cprintf("");getch();break;
+			case '1':
+         cprintf("");
+{
+    bool p, q;
+
+    cout<<"Introducir el el primer valor de verdad   :";
+
+    cin>>p;
+
+    cout<<"Introduce el  segundo valor de verdad   :";
+
+    cin>>q;
+
+    cout<<"Introducir la operacion logica :    ";
+
+    cout<<" 1-----> OR"<<"   2------>AND   ";
+
+    int unsigned operacion_logica;
+
+    cin>>operacion_logica;cout<<"  EL VALOR DE VERDAD ES :  ";
+
+    bool resultado;
+    switch (operacion_logica)
+    {
+
+        case (1):
+            resultado  = (p||q);
+            break;
+
+        case (2):
+            resultado  = (p&&q);
+            break;
+    }
+    cout << resultado << endl;
+    getch() ;
+}
+
+         getch();break;
 			case '2': cprintf("");getch();break;
 			case '3': cprintf("");getch();break;
          case '4': repetir=0;break;
